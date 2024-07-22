5c04de0a6c8cd002072dd77e92559881baeda260
n-inelie <nin3li3@yahoo.com>
2024-07-22 18:59:24 +1000

    factorial bug fixed

--- src/mathex.c
+++ src/mathex.c
@@ -1,8 +1,8 @@
 #include "mathex.h"
 
 double factorial(double x) {
-    if (x == 0) {
-        return 1;
+    if (x == 0.0) {
+        return 1.0;
     }
-    return x * factorial(x);
+    return x * factorial(x - 1);
 }
