38ec6335cbd53d76dccd490b0c0f25f9098a5172
n-inelie <nin3li3@yahoo.com>
2024-07-23 15:36:33 +1000

    tidied filet_token_stack

--- src/token-stack-filter.c
+++ src/token-stack-filter.c
@@ -3,6 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+static inline size_t handleRParen(TokenStack *t_stack, size_t i, Token *t_new) {
+    size_t j = i - 1;
+    size_t r_paren_count = 1;
+    size_t l_paren_count = 0;
+    while (j > 0) {
+        j--;
+        if (t_stack->tokens[j].type == R_PAREN) {
+            r_paren_count++;
+        }
+        if (t_stack->tokens[j].type == L_PAREN) {
+            l_paren_count++;
+            if (l_paren_count == r_paren_count) {
+                break;
+            }
+        }
+    }
+    memmove(t_stack->tokens + j + 1, t_stack->tokens + j,
+            sizeof(Token) * (i - j));
+    t_stack->tokens[j] = *t_new;
+    return 0;
+}
+
 void filter_token_stack(TokenStack *t_stack) {
     Token t;
     Token t_new;
@@ -36,31 +58,14 @@ void filter_token_stack(TokenStack *t_stack) {
             default:
                 break;
             }
-            if (temp.type == R_PAREN) {
-                size_t j = i - 1;
-                size_t r_paren_count = 1;
-                size_t l_paren_count = 0;
-                while (j > 0) {
-                    j--;
-                    if (t_stack->tokens[j].type == R_PAREN) {
-                        r_paren_count++;
-                    }
-                    if (t_stack->tokens[j].type == L_PAREN) {
-                        l_paren_count++;
-                        if (l_paren_count == r_paren_count) {
-                            break;
-                        }
-                    }
-                }
-                memmove(t_stack->tokens + j + 1, t_stack->tokens + j,
-                        sizeof(Token) * (i - j));
-                t_stack->tokens[j] = t_new;
-                continue;
-            } else if (t_stack->tokens[i - 1].type == L_PAREN) {
+            if (t_stack->tokens[i - 1].type == L_PAREN) {
                 fprintf(
                     stderr,
                     "Error: Invalid positioning of openining parenthesis '('");
                 exit(EXIT_FAILURE);
+            }
+            if (temp.type == R_PAREN) {
+                handleRParen(t_stack, i, &t_new);
             }
             t_stack->tokens[i - 1] = t_new;
             t_stack->tokens[i] = temp;
